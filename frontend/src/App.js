268424610c7b7c6efdfdcfb208610f2587aadb7c
prat345 <pratprasert@gmail.com>
2023-11-10 19:56:51 +0700

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazon</a>
+        <a href="/product">products</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

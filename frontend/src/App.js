d39d248befbe849ec13ebc85154d3e42a22becd1
prat345 <pratprasert@gmail.com>
2023-11-10 20:45:50 +0700

    1st product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,25 @@
 import "./App.css";
+import data from "./data.js";
 
 function App() {
   return (
     <div>
       <header>
         <a href="/">amazon</a>
-        <a href="/product">products</a>
+        <a href="/product">product</a>
       </header>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="product-container">
+          {data.product.map((product) => (
+            <div className="product">
+              <img src={product.image} alt={product.name} />
+              <p>{product.name}</p>
+              <p>{product.price}</p>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }

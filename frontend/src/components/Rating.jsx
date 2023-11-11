5e04ce31a4b3ef06daf61ca500707cb43a8f33c0
prat345 <pratprasert@gmail.com>
2023-11-11 20:01:17 +0700

    add rating

---
+++ frontend/src/components/Rating.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+export default function Rating(props) {
+  const { rating, numReviews } = props;
+  return (
+    <div className="rating">
+      <span>
+        {/* Stars icon from font awesome
+        cdn in public/index.html */}
+        <i
+          className={
+            rating >= 1
+              ? "fas fa-star" // full star
+              : rating >= 0.5
+              ? "fas fa-star-half-alt" // half star
+              : "far fa-star" // empty star
+          }
+        ></i>
+        <i
+          className={
+            rating >= 2
+              ? "fas fa-star"
+              : rating >= 1.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+        <i
+          className={
+            rating >= 3
+              ? "fas fa-star"
+              : rating >= 2.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+        <i
+          className={
+            rating >= 4
+              ? "fas fa-star"
+              : rating >= 3.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+        <i
+          className={
+            rating >= 5
+              ? "fas fa-star"
+              : rating >= 4.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>{numReviews}</span>
+    </div>
+  );
+}
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useReducer, useState } from "react";
-import data from "../data";
+// import data from "../data"; // mock data
 import { Link } from "react-router-dom";
 import axios from "axios";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Product from "../components/Product";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -41,20 +44,13 @@ export default function HomePage() {
     <div>
       <h1>Featured Products</h1>
       <div className="product-container">
-        {products.map((product) => (
-          <div className="product" key={product.slug}>
-            <Link to={`product/${product.slug}`}>
-              <img src={product.image} alt={product.name} />
-            </Link>
-            <div className="product-info">
-              <Link to={`product/${product.slug}`}>
-                <p>{product.name}</p>
-              </Link>
-              <p>${product.price}</p>
-              <button>Add to cart</button>
-            </div>
-          </div>
-        ))}
+        <Row>
+          {products.map((product) => (
+            <Col key={product.slug} sm={6} md={4} lg={3}>
+              <Product product={product}></Product>
+            </Col>
+          ))}
+        </Row>
       </div>
     </div>
   );

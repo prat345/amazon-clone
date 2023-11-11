8e790afbe94522a278c1781a657e8a9c8652d437
prat345 <pratprasert@gmail.com>
2023-11-11 19:17:16 +0700

    add bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,36 @@ import "./App.css";
 import HomePage from "./pages/HomePage.jsx";
 import ProductPage from "./pages/ProductPage.jsx";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">amazon</Link>
-          <Link to="/product">product</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Amazon</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+          {/* <Link to="/">amazon</Link>
+              <Link to="/product">product</Link> */}
         </header>
         <main>
-          <Routes>
-            <Route exact path="/" element={<HomePage />} />
-            <Route path="/product/:slug" element={<ProductPage />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route exact path="/" element={<HomePage />} />
+              <Route path="/product/:slug" element={<ProductPage />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useReducer, useState } from "react";
 import data from "../data";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import logger from "use-reducer-logger"; // track reducer in browser console
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -19,7 +18,7 @@ const reducer = (state, action) => {
 
 export default function HomePage() {
   // const [products, setProducts] = useState([]);
-  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
     products: [],
     loading: true,
     error: "",

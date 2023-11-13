edb5f8b4401465452d8fea316f69d25fa6282120
prat345 <pratprasert@gmail.com>
2023-11-13 23:31:49 +0700

    mongo setup

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Amazon Clone MERN-stack project
 
-* https://www.youtube.com/watch?v=CDtPMR5y0QU&t=6519s
-* https://github.com/basir/mern-amazona/tree/master/frontend/src
-* https://amazona.onrender.com/
+- https://www.youtube.com/watch?v=CDtPMR5y0QU&t=6519s
+- https://github.com/basir/mern-amazona/tree/master/frontend/src
+- https://amazona.onrender.com/
+
+requirement
+
+package.json > "type": "module"
+
+cd frontend
+npm i react-router-dom axios react-bootstrap bootstrap react-helmet-async
+
+cd backend
+npm i express mongoose dotenv
--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,25 @@
 import express from "express";
 import data from "./data.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoute.js";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to Mongo");
+  })
+  .catch((err) => {
+    console.error(err.message);
+  });
 
 const app = express();
 
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
-
-app.get("/api/products/slug/:slug", (req, res) => {
-  const product = data.products.find((x) => x.slug === req.params.slug);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
+app.use("/api/seed", seedRouter); // http://localhost:5000/api/seed
+app.use("/api/products", productRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () =>

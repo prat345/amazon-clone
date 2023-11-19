0e47125a8742fcbd851cb45b0d4747842b9363c0
prat345 <pratprasert@gmail.com>
2023-11-19 23:28:01 +0700

    build deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import data from "./data.js";
+import path from "path";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import seedRouter from "./routes/seedRoutes.js";
@@ -31,6 +32,12 @@ app.use("/api/seed", seedRouter); // http://localhost:5000/api/seed > create sam
 app.use("/api/products", productRouter); // http://localhost:5000/api/products
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
+});
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });

417d014e6103cbf9de44ab3555de2aca32c00cbb
prat345 <pratprasert@gmail.com>
2023-12-29 06:54:24 +0700

    rewrites 2

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ app.get("/api/keys/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || "sandbox");
 });
 
-app.use("/api/seed", seedRouter); // http://localhost:5000/api/seed > create sample product in db
+app.use("/api/seed", seedRouter); // http://localhost:5000/api/seed > create sample product in DB
 app.use("/api/products", productRouter); // http://localhost:5000/api/products
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);

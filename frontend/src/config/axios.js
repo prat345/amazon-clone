52648b824a90a520bafa9c951bcf2956b75d6ecc
prat345 <pratprasert@gmail.com>
2023-12-30 06:27:51 +0700

    set up vercel

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// axios.defaults.baseURL = "https://amazon-clone-backend-chi.vercel.app/";
+axios.defaults.baseURL = "https://amazon-clone-backend-chi.vercel.app/";
 // axios.defaults.baseURL = "http://localhost:5000";
 // axios.defaults.withCredentials = true;
 

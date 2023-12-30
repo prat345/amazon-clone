305173b6fb45904f74dfbb96c4fd336808f6ac12
prat345 <pratprasert@gmail.com>
2023-12-30 07:54:24 +0700

    vercel backend,frontend

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,10 +1,7 @@
 import axios from "axios";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 // axios.defaults.baseURL = "https://amazon-clone-backend-chi.vercel.app";
-axios.defaults.baseURL = process.env.API_BASE_URL || "http://localhost:5000";
+axios.defaults.baseURL = "http://localhost:5000";
 axios.defaults.withCredentials = true;
 
 export default axios;

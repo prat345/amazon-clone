256459d0ac426079115a42c00bf553ae30bef57f
prat345 <pratprasert@gmail.com>
2023-12-30 08:06:31 +0700

    fix base api

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // axios.defaults.baseURL = "https://amazon-clone-backend-chi.vercel.app";
-axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL = "https://amazon-clone-nu-gilt.vercel.app/";
 axios.defaults.withCredentials = true;
 
 export default axios;
